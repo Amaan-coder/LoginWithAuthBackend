0eb14629dbcd3a7f7538ce560bcfa4450c71b1bf
Mohd Amaan <mohd.amaan45@gmail.com>
2024-02-26 20:31:10 +0530

    Project Added

---
+++ src/main/java/com/springauth/LoginWithAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithAuthenticationApplication.class, args);
+	}
+
+}
